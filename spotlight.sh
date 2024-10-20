1b0dc9b72152f631c7aa6a6b8c6b6830cd20da19
Lukas Tesar <lukastesar03@gmail.com>
2024-10-20 18:11:58 +0200

    fix: fail on first startup

--- spotlight.sh
+++ spotlight.sh
@@ -72,7 +72,7 @@ qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
 
 mkdir -p "$spotlightPath"
 
-previousImagePath="$(readlink "$spotlightPath/background.jpg")"
+previousImagePath="$(readlink "$spotlightPath/background.jpg" || true)"
 ln -sf "$imagePath" "$spotlightPath/background.jpg"
 
 if [ "$keepImage" = false ] && [ -n "$previousImagePath" ] && [ -f "$previousImagePath" ] && [ "$imagePath" != "$previousImagePath" ]
